e40e31d8795a296032cd1cc0ec70797f3f747837
cindy <cindy@kaludeMacBook-Air.local>
2015-03-08 17:52:02 +0800

    Initial Commit

---
+++ iOSgithub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSgithub
+//
+//  Created by cindy on 2015/3/8.
+//  Copyright (c) 2015年 cindy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOSgithub/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSgithub
+//
+//  Created by cindy on 2015/3/8.
+//  Copyright (c) 2015年 cindy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
